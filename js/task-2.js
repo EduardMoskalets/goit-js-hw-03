8dbe41bee4c9c02a87b0c2e35f1c69fd896447c0
Eduard <edik.moskalets@gmail.com>
2024-04-21 13:09:00 +0300

    upd task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,8 @@
-function makeArray ()
+function makeArray(firstArray, secondArray, maxLength) {
+    let newArray = [firstArray, secondArray];
+    if (maxLength > newArray) {
+        newArray = newArray.slice(0, maxLength);
+    } 
+    return newArray;
+}
+
