6e29af2b6cfe8b1273b8f8a1e1048e7e696013b8
Eduard <edik.moskalets@gmail.com>
2024-04-21 13:03:21 +0300

    upd task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function slugify(title) {
-    return title.toLowerCase().trim().replace(/\s+/g, '-');
+   return title.toLowerCase().split(' ').join('-');
 }
 
 console.log(slugify("Arrays for begginers"));
